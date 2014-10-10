0833a9d1f177604bd39b085fe7f2cf06f246c2e3
Hank Duan <duan.hank@gmail.com>
2014-10-10 14:25:42 -0700

    22

--- scripts/testonsauce.sh
+++ scripts/testonsauce.sh
@@ -1,13 +1,14 @@
 SAUCE_ACCESS_KEY=`echo $SAUCE_ACCESS_KEY | rev`
 
-if [ $JOB = "smoke" ]; then
+if [ "$JOB" = "smoke" ]; then
   node bin/protractor spec/smokeConf.js
-elif [ $JOB = "suite" ]; then
+elif [ "$JOB" = "suite" ]; then
   node bin/protractor spec/ciConf.js
-elif [ $JOB = "special" ]; then
-  out=`node bin/protractor spec/specialConf.js`
+elif [ "$JOB" = "special" ]; then
+  node bin/protractor spec/specialConf.js
+  out=$?
   echo $out
-  if [ $out = "0" ]; then
+  if [ $out -eq "0" ]; then
     echo "successful"
   else
     echo "FAIL"
