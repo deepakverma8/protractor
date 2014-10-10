7a46d99ce6cf8e354d1f06d78643978e634c1d68
Hank Duan <duan.hank@gmail.com>
2014-10-10 14:03:27 -0700

    hank1

--- scripts/testonsauce.sh
+++ scripts/testonsauce.sh
@@ -4,6 +4,14 @@ if [ $JOB = "smoke" ]; then
   node bin/protractor spec/smokeConf.js
 elif [ $JOB = "suite" ]; then
   node bin/protractor spec/ciConf.js
+elif [ $JOB = "special" ]; then
+  out=`node bin/protractor spec/specialConf.js`
+  echo $out
+  if [ $out = "0" ]; then
+    echo "successful"
+  else
+    echo "FAIL"
+  fi
 else
   echo "Unknown job type. Please set JOB=smoke or JOB=suite"
 fi
---
+++ spec/specialConf.js
@@ -0,0 +1,44 @@
+var env = require('./environment.js');
+
+// The main suite of Protractor tests to be run on CI servers.
+exports.config = {
+  sauceUser: process.env.SAUCE_USERNAME,
+  sauceKey: process.env.SAUCE_ACCESS_KEY,
+
+  // Spec patterns are relative to this directory.
+  specs: [
+    'basic/actions_spec.js'
+  ],
+
+  multiCapabilities: [{
+    'browserName': 'chrome',
+    'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
+    'build': process.env.TRAVIS_BUILD_NUMBER,
+    'name': 'Protractor suite tests',
+    'version': '36',
+    'platform': 'OS X 10.9',
+    'selenium-version': '2.43.1'
+  }, {
+    'browserName': 'firefox',
+    'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
+    'build': process.env.TRAVIS_BUILD_NUMBER,
+    'name': 'Protractor suite tests',
+    'version': '32',
+    'selenium-version': '2.43.1'
+  }],
+
+  baseUrl: env.baseUrl,
+
+  jasmineNodeOpts: {
+    isVerbose: true,
+    showTiming: true,
+    defaultTimeoutInterval: 90000
+  },
+
+  params: {
+    login: {
+      user: 'Jane',
+      password: '1234'
+    }
+  }
+};
