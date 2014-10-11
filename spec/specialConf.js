3c94ab8d48cb7189e51431164905b56c7d228197
Hank Duan <duan.hank@gmail.com>
2014-10-10 17:47:30 -0700

    2

--- spec/specialConf.js
+++ spec/specialConf.js
@@ -1,4 +1,5 @@
 var env = require('./environment.js');
+var start;
 
 // The main suite of Protractor tests to be run on CI servers.
 exports.config = {
@@ -7,25 +8,28 @@ exports.config = {
 
   // Spec patterns are relative to this directory.
   specs: [
-    'basic/actions_spec.js'
+    'basic/timeout_spec.js'
   ],
 
   multiCapabilities: [{
-    'browserName': 'chrome',
-    'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
-    'build': process.env.TRAVIS_BUILD_NUMBER,
-    'name': 'Protractor suite tests',
-    'version': '36',
-    'platform': 'OS X 10.9',
-    'selenium-version': '2.43.1'
-  }, {
-    'browserName': 'firefox',
-    'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
-    'build': process.env.TRAVIS_BUILD_NUMBER,
-    'name': 'Protractor suite tests',
-    'version': '32',
-    'selenium-version': '2.43.1'
+    'browserName': 'chrome'
   }],
+  // multiCapabilities: [{
+  //   'browserName': 'chrome',
+  //   'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
+  //   'build': process.env.TRAVIS_BUILD_NUMBER,
+  //   'name': 'Protractor suite tests',
+  //   'version': '36',
+  //   'platform': 'OS X 10.9',
+  //   'selenium-version': '2.43.1'
+  // }, {
+  //   'browserName': 'firefox',
+  //   'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
+  //   'build': process.env.TRAVIS_BUILD_NUMBER,
+  //   'name': 'Protractor suite tests',
+  //   'version': '32',
+  //   'selenium-version': '2.43.1'
+  // }],
 
   baseUrl: env.baseUrl,
 
@@ -35,10 +39,14 @@ exports.config = {
     defaultTimeoutInterval: 90000
   },
 
-  params: {
-    login: {
-      user: 'Jane',
-      password: '1234'
-    }
-  }
+  onPrepare: function() {
+    start = new Date().getTime();
+  },
+
+  onCleanUp: function(exitCode) {
+    var duration = new Date().getTime() - start;
+    console.log('duration is: ', duration)
+    //expect(duration > 4900 && duration < 6000).toBeTruthy();
+    return 0;
+  },
 };
